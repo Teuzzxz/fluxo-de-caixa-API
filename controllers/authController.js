87114e1c2b6a58253ba53eedb338ea99fb77f78d
Teuzzxz <mateusramosmrpc@gmail.com>
2025-11-29 14:56:29 -0300

    tentativa 6

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,18 @@
 import { registerService } from "../services/authService.js"
 
 export async function registerController(err, req, res) {
-   console.log(err)
-   res.status(500).json({ ok: false, menssager: "Erro interno do servidor" })
+   console.log("1 - entrou no controler")
    try {
+      console.log("2 - entrou no try")
       const { email, password } = req.body
+      console.log("3 - pegou as reqs do body")
+
       const result = await registerService(email, password, req)
 
       if (!result.ok) {
          res.status(400).json({ ok: false, menssager: "Ja existe" })
       }
+
       if (result.ok) {
          res.status(200).json({ ok: "true", menssager: "Criado com sucesso" })
       }
--- services/authService.js
+++ services/authService.js
@@ -3,15 +3,22 @@ import { hashPassword } from "../utils/passwordHASH.js"
 // import { getLocation } from "../utils/geoLocation.js"
 
 export const registerService = async (email, password, req) => {
+   console.log("4 - Entrou no auth")
+
    const exist = await Users.findOne({ email })
    if (exist) {
       return {
          ok: false,
       }
    }
+
+   console.log("5 - Pesquisou no banco de dados")
+
    const hash = await hashPassword(password)
 
-   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || "187.44.10.20"
+   console.log("6 - Fez a senha")
+
+   // const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || "187.44.10.20"
 
    // const location = await getLocation(ip)
 
@@ -24,6 +31,8 @@ export const registerService = async (email, password, req) => {
       // state: location.state,
       // country: location.country,
    })
+
+   console.log("7 - Cadastrou o user")
 
    return {
       ok: true,
