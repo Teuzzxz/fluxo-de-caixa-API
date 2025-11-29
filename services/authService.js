03442d349aa0c2711e939ac975293f05df6cf5f0
Teuzzxz <mateusramosmrpc@gmail.com>
2025-11-29 14:44:26 -0300

    tentativa 5

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,6 @@
 import Users from "../mongodbSchemas/users.js"
 import { hashPassword } from "../utils/passwordHASH.js"
-import { getLocation } from "../utils/geoLocation.js"
+// import { getLocation } from "../utils/geoLocation.js"
 
 export const registerService = async (email, password, req) => {
    const exist = await Users.findOne({ email })
@@ -13,16 +13,16 @@ export const registerService = async (email, password, req) => {
 
    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || "187.44.10.20"
 
-   const location = await getLocation(ip)
+   // const location = await getLocation(ip)
 
    const newUser = await Users.create({
       email: email,
       password: hash,
       role: "user",
       ipCreated: ip,
-      city: location.city,
-      state: location.state,
-      country: location.country,
+      // city: location.city,
+      // state: location.state,
+      // country: location.country,
    })
 
    return {
