8c390cab6b656046f46718f294cd103c8775768e
Teuzzxz <mateusramosmrpc@gmail.com>
2025-11-29 15:29:48 -0300

    teste final

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,8 @@
 import { registerService } from "../services/authService.js"
 
 export async function registerController(req, res) {
-   console.log("1 - entrou no controler")
-   console.log("requisições recebidas, tentando ler:" + req.body)
    try {
-      console.log("2 - entrou no try")
       const { email, password } = req.body
-      console.log("3 - pegou as reqs do body")
 
       const result = await registerService(email, password, req)
 
@@ -18,7 +14,6 @@ export async function registerController(req, res) {
          res.status(200).json({ ok: "true", menssager: "Criado com sucesso" })
       }
    } catch (error) {
-      console.log("4.1 - parou no primeiro error")
-      // res.status(400).json({ ok: false, menssager: error.message })
+      res.status(400).json({ ok: false, menssager: error.message })
    }
 }
--- services/authService.js
+++ services/authService.js
@@ -4,8 +4,6 @@ import { getLocation } from "../utils/geoLocation.js"
 import { getClientIp } from "../utils/getip.js"
 
 export const registerService = async (email, password, req) => {
-   console.log("4 - Entrou no auth")
-
    const exist = await Users.findOne({ email })
    if (exist) {
       return {
@@ -13,28 +11,21 @@ export const registerService = async (email, password, req) => {
       }
    }
 
-   console.log("5 - Pesquisou no banco de dados")
-
    const hash = await hashPassword(password)
 
-   console.log("6 - Fez a senha")
-
    const location = await getLocation(getClientIp(req))
-
-   console.log(location)
-   console.log("Acima tem a localização que pegou")
 
    const newUser = await Users.create({
       email: email,
       password: hash,
       role: "user",
-      // ipCreated: ip,
-      // city: location.city,
-      // state: location.state,
-      // country: location.country,
+      ipCreated: getClientIp(req),
+      city: location.city,
+      state: location.state,
+      country: location.country,
    })
 
-   console.log("7 - Cadastrou o user")
+   console.log(newUser)
 
    return {
       ok: true,
