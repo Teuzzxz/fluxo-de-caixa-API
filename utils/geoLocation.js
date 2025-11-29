6e050783c9845e8f51375f078dfc0cb08e42a074
Teuzzxz <mateusramosmrpc@gmail.com>
2025-11-29 15:14:51 -0300

    tentativa 11

--- utils/geoLocation.js
+++ utils/geoLocation.js
@@ -1,12 +1,15 @@
-const fetch = (await import("node-fetch")).default
-
 export async function getLocation(ip) {
-   const response = await fetch(`https://ipapi.co/${ip}/json/`)
-   const data = await response.json()
-
-   return {
-      city: data.city,
-      state: data.region,
-      country: data.country_name,
+   try {
+      const response = await fetch(`https://ipapi.co/${ip}/json/`)
+      if (!response.ok) throw new Error("Falha ao obter IP")
+      const data = await response.json()
+      return {
+         city: data.city,
+         state: data.region,
+         country: data.country_name,
+      }
+   } catch (err) {
+      console.error(err)
+      return { city: null, state: null, country: null }
    }
 }
