ee32a3ba698808e2597fc196345951c5e8d81c5e
Teuzzxz <mateusramosmrpc@gmail.com>
2025-12-03 14:31:42 -0300

    teste 1

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,6 +5,8 @@ const JWT_SECRET = process.env.JWT_SECRET
 export default function authMiddleware(req, res, next) {
    const token = req.cookies.access_token
 
+   console.log(token)
+
    if (!token) {
       console.log("Token nao fornecido")
       return res.status(400).json({ ok: false, message: "Token não fornecido" })
@@ -12,6 +14,8 @@ export default function authMiddleware(req, res, next) {
 
    try {
       const decoded = jwt.verify(token, JWT_SECRET)
+
+      console.log(decoded)
 
       req.user = decoded
 
