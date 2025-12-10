ba289ff80f38f7cc90aa812b53710d8881785b82
Teuzzxz <mateusramosmrpc@gmail.com>
2025-12-10 20:11:43 -0300

    tentativa 2

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ export default function authMiddleware(req, res, next) {
    const token = req.cookies.access_token
 
    if (!token) {
-      return res.status(200).json({ ok: false, message: "Token não fornecido" })
+      return res.status(400).json({ ok: false, message: "Token não fornecido" })
    }
 
    try {
